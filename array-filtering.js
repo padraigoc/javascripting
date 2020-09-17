406ab7c0414d29344ffe5c0d7c26ca29a5d35c95
Patrick <dev.padraig@gmail.com>
2020-09-17 02:35:05 +0000

    Pushing files to github

---
+++ array-filtering.js
@@ -0,0 +1,17 @@
+let numbers = [1,2,3,4,5,6,7,8,9,10]
+let filtetered = numbers.filter(function (number) {
+  return(number % 2===0)
+});
+
+// function evenNumbers (number) {
+// return number % 2 === 0
+
+// }
+
+// for(var i=0; i<numbers.length; i++){
+// console.log(evenNumbers(numbers[i]))
+// }
+
+console.log(filtetered)
+
+
